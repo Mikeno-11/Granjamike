f35ea72964c41e99849c7ccc3df8922b120f34a5
Mikeno-11 <miguel.lopez@ucic.edu.mx>
2025-07-14 11:19:01 -0600

    Initial commit

---
+++ Assets/Scripts/GallinaMove.cs
@@ -0,0 +1,63 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Gallina : MonoBehaviour
+{
+    public Transform areaMovimiento;
+    public float velocidad;
+    private Vector2 destino;
+    public SpriteRenderer sr;
+    Vector2 posicionActual;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        NuevoDestino();
+        posicionActual = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, destino, velocidad * Time.deltaTime);
+        
+        Vector2 direccion = posicionActual - destino;
+        Debug.Log("Direccion" + direccion.x);
+
+        if (direccion.x >=0)
+        {
+            sr.flipX = true;
+        }
+
+        else
+        {
+            sr .flipX = false;
+        }
+        
+
+        if(Vector2.Distance (transform.position,destino)<0.1)
+        {
+            NuevoDestino();
+            posicionActual = transform .position;
+      
+        }
+
+    }
+    void NuevoDestino()
+    {
+        Vector2 centro = areaMovimiento.position;
+        Vector2 tamaño = areaMovimiento.localScale;
+
+        float x = Random.Range(centro.x - tamaño.x / 2f, centro.x + tamaño.x / 2f);
+        float y = Random.Range(centro.y - tamaño.y / 2f, centro.y + tamaño.y / 2f);
+
+        destino = new Vector2(x, y);
+
+        Debug.Log(destino);
+
+    }
+
+}
